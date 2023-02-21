589fa8e55fc4a6d472a1bf47599937f9079672cc
NguyenSilverN <hoangducnguyen3@gmail.com>
2023-02-21 14:09:34 +0700

    add class and gender

--- msglog.php
+++ msglog.php
@@ -1,57 +1,59 @@
 <?php
-    // Kết nối database, lấy dữ liệu chung
     include('includes/general.php');
-
-    // Lấy dữ liệu từ table messages theo thứ tự id_msg tăng dần
     $query_get_msg = mysqli_query($cn, "SELECT * FROM messages ORDER BY id_msg ASC");
-
-    // Tạo một mảng để lưu trữ tất cả các tin nhắn
     $messages = array();
-
-    // Dùng vòng lập while để lấy dữ liệu và lưu vào mảng
-    while ($row = mysqli_fetch_assoc($query_get_msg))
-    {
+    while ($row = mysqli_fetch_assoc($query_get_msg)){
         array_unshift($messages, $row);
     }
 
     // Duyệt qua mảng các tin nhắn và hiển thị
+    $male_count = 0;
+    $female_count = 0;
+    $total_students = 0;
+    $hoten = '';
+    $chihoi = '';
     foreach ($messages as $row)
     {
-        // Thời gian điểm danh
-        $date_sent = $row['date_sent'];
-        $day_sent = substr($date_sent, 8, 2); // Ngày gửi
-        $month_sent = substr($date_sent, 5, 2); // Thánh gửi
-        $year_sent = substr($date_sent, 0, 4); // Năm gửi
-        $hour_sent = substr($date_sent, 11, 2); // Giờ gửi
-        $min_sent = substr($date_sent, 14, 2); // Phút gửi
-
         echo '<div class="msg-user">';
 
         // Lấy thông tin tên sinh viên dựa trên MSSV
         $student_mssv = $row['body'];
-        $query = mysqli_query($cn, "SELECT original_data.hoten FROM messages JOIN original_data ON messages.body = original_data.mssv WHERE messages.body = '$student_mssv'");
+        $query = mysqli_query($cn, "SELECT original_data.hoten, original_data.gioitinh, original_data.chihoi FROM messages JOIN original_data ON messages.body = original_data.mssv WHERE messages.body = '$student_mssv'");
         $result = mysqli_fetch_assoc($query);
-        $total_students = mysqli_num_rows(mysqli_query($cn, "SELECT * FROM messages"));
 
         if (!empty($result)) {
             $hoten = $result['hoten'];
+            $gender = $result['gioitinh'];
+            $chihoi = $result['chihoi'];
             // insert into the top of the table
-             echo " $student_mssv - $hoten";
+            echo " $student_mssv - $hoten -$chihoi ";
+
+            // increment the count of male or female students based on the gender
+            if ($gender == 'Nam') {
+                $male_count++;
+            } else {
+                $female_count++;
+            }
+            
+
         } else {
             echo " $student_mssv - Bạn không phải là đại biểu chính thức.";
-            // if total_student > 0, deincrement total_students
             $total_students--;
         }
 
-        // Hiển thị thông tin tin nhắn
-        // echo '<div class="info-msg-user">';
-        // // echo 'Điểm danh lúc '.$hour_sent.':'.$min_sent.' ngày '.$day_sent.'/'.$month_sent.'/'.$year_sent.'.';
-        // echo '</div>';
         echo '</div>';
+        $total_students++;
+        // Hiển thị tên sinh viên
+
+
     }
 
-    // Tính số sinh viên đã điểm danh
-    
     // Hiển thị tổng số sinh viên
-    echo '<h1 class="box-sum-item-title" id="total-students">Tổng số đại biểu: ' . $total_students . '</h1>';
+    echo '<h1 class="box-sum-item-title box-sum-item-title1" id="total-students">Tổng số đại biểu: ' . $total_students . '</h1>';
+    // Hiển thị số lượng nam và nữ trong danh sách
+    echo '<h1 class="box-sum-item-title box-sum-item-title2" id="male-students">Số lượng nam: ' . $male_count . '</h1>';
+    echo '<h1 class="box-sum-item-title box-sum-item-title3" id="female-students">Số lượng nữ: ' . $female_count . '</h1>';
+
+
+
 ?>
